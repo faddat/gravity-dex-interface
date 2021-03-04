3c71daf6660cda810cb0ca104d81896035e0bf4b
sunghoon <sunghoon@bharvest.io>
2021-03-04 14:31:12 +0900

    [fix] refactor button

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled from "styled-components";
 import BaseCard from "../../components/Cards/BaseCard"
+import GearButton from "../../components/Buttons/Gear"
 
 const Swap = styled.div`
     .header {
@@ -11,21 +12,6 @@ const Swap = styled.div`
         .title {
             padding-left: 4px;
             font-weight: 500;
-        }
-
-        .setting {
-            .gear-button {
-                width: 24px;
-                height: 24px;
-                padding: 0;
-                border: none;
-                background-color: transparent;
-                opacity: 0.8;
-                cursor:pointer;
-                &:hover {
-                    opacity: 0.5;
-                }
-            }
         }
     }
 `
@@ -44,10 +30,7 @@ function Deposit() {
                         Swap
                     </div>
                     <div className="setting">
-                        <button className="gear-button">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="sc-gbOuXE daxFHC">
-                                <circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
-                        </button>
+                        <GearButton />
                     </div>
                 </div>
             </Swap>
