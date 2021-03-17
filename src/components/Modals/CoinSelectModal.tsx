2c31cc4ba07a6c07f47561819cb46d3230b9d0f8
sunghoon <sunghoon@bharvest.io>
2021-03-17 21:30:57 +0900

    [feat] add pool data

--- src/components/Modals/CoinSelectModal.tsx
+++ src/components/Modals/CoinSelectModal.tsx
@@ -1,6 +1,8 @@
-import * as React from 'react';
-import BasicModal from "./BasicModal";
-import styled from "styled-components";
+import * as React from 'react'
+import { useDispatch, useSelector } from "react-redux"
+
+import BasicModal from "./BasicModal"
+import styled from "styled-components"
 
 const Title = styled.div`
 width: 300px;
@@ -19,6 +21,8 @@ padding: 60px 12px 40px ;
 `
 
 function CoinSelectModal({ isOpen, toggle }: { isOpen: boolean, toggle: any }) {
+    const PoolsData = useSelector((state) => state.store.poolsData)
+    console.log(PoolsData)
 
     return (
 
