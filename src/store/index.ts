152aa8282bc8b92c34f2b5fe6d0b7035a22ab5cb
sunghoon <sunghoon@bharvest.io>
2021-03-17 18:31:52 +0900

    [test] add test data pools format

--- src/store/index.ts
+++ src/store/index.ts
@@ -15,14 +15,20 @@ const TEST_INIT_DATA = {
             band: 500,
         }
     },
-    poolData: {
-
+    //헬퍼 함수 하나 빼서 getPool(a, b) pair 하나씩 넣고 안에서 알파벳 순으로 솔팅 돌리고 하면 될듯
+    poolsData: {
+        pairs: ['atom', 'iris', 'band', 'luna', 'kava'],
+        pools: {
+            "atom-iris": { atom: 1000, iris: 1003 },
+            "atom-band": { atom: 871.123, band: 111.31221 },
+            "band-luna": { band: 233333, luna: 12234.23124 }
+        }
     }
 }
 
 
 export const RootReducer = createSlice({
-    name: "todoList",
+    name: "rootData",
     initialState: TEST_INIT_DATA,
     reducers: {},
 });
